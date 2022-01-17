126da2f2c466380a94d3ecae91b4a264c927077c
Robbie Weld <43070503+robweld@users.noreply.github.com>
2022-01-17 11:52:29 +0000

    Update teleop.py

--- packages/teleop/scripts/teleop.py
+++ packages/teleop/scripts/teleop.py
@@ -1,49 +1,63 @@
 #!/usr/bin/env python
 
+import sys
 import rospy
 from std_msgs.msg import String
 from pynput import keyboard
-from pynput.keyboard import Key, Listener
-import sys
+from pynput.keyboard import Key, Listener, KeyCode
 
-def talker(my_msg):
-    rospy.loginfo(my_msg)
-    pub.publish(my_msg)
+class Teleop:
+    def __init__(self, key_list, topic_name='key_press'):
+        self.key_list = key_list        
+        self.topic_name = topic_name
+        
+        self.pressed_key_list = []
+        self.pub = rospy.Publisher(topic_name, String, queue_size=1)
 
-def on_press(key):
-    global pressed
-    global selected_key
-    if selected_key == Key.esc:
-        selected_key = key
-        print("Listening to {0}".format(key))
-    elif key == selected_key and pressed == False:
-        pressed = True
-        my_msg = "pressed {0} key".format(key)
-        talker(my_msg)
+        with Listener(
+                on_press=self.on_press,
+                on_release=self.on_release) as listener:
+            listener.join()
 
-def on_release(key):
-    global pressed
-    if pressed == True:
-        if key == selected_key or key == Key.esc:
-            pressed = False
-            my_msg = "released {0} key".format(key)
-            talker(my_msg)
-    if key == Key.esc:
-        return False
+    def on_press(self, key):
+        msg = '{0}_p'.format(key)
+        try:
+            if key.char in self.key_list:
+                if key.char not in self.pressed_key_list:
+                    self.pressed_key_list.append(key.char)                
+                    self.publish(msg)
+        
+        except AttributeError:
+            if key in self.key_list:
+                if key.char not in self.pressed_key_list:
+                    self.pressed_key_list.append(key)                
+                    self.publish(msg)
+
+
+    def on_release(self, key):
+        msg = '{0}_r'.format(key)
+        try:
+            if key.char in self.key_list:
+                self.pressed_key_list.remove(key.char)
+                self.publish(msg)
+        
+        except AttributeError:
+            if key == keyboard.Key.esc:                
+                return False    # Stop listener
+            if key in self.key_list:
+                self.pressed_key_list.remove(key)
+                self.publish(msg)
+    
+    def publish(self, msg):
+        rospy.loginfo(msg)
+        self.pub.publish(msg)
+        
 
 if __name__ == '__main__':
-    if sys.argv[1] == None:
-        print("add name for publisher as an argument")
-        sys.exit()
     try:
-        pressed = False
-        selected_key = Key.esc
-        print("Press esc to exit")
-        print("Publishes selected key press and release")
-        print("press any key to start listening")
-        pub = rospy.Publisher('chatter', String, queue_size=10)
-        rospy.init_node(sys.argv[1], anonymous=True)
-        with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-            listener.join()
+        rospy.init_node('teleop_node', anonymous=True)
+        key_list = ['a', 'd', 's', 'w', Key.space]
+        t = Teleop(key_list)
     except rospy.ROSInterruptException:
         pass
+
