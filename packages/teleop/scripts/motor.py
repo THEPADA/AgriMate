f86f6a5acf30c21bd2329d401e37fbf8f73cdb6a
Vijja Wichitwechkarn <58631600+vijja-w@users.noreply.github.com>
2022-01-18 16:06:20 +0000

    improvements

--- packages/teleop/scripts/motor.py
+++ packages/teleop/scripts/motor.py
@@ -1,31 +1,32 @@
-from DRV8825 import DRV8825
 import threading
+from DRV8825 import DRV8825
+
 
 class Motor():
-    def __init__(self, dir_pin, step_pin, enable_pin, mode_pins):
+    def __init__(self, dir_pin, step_pin, enable_pin, mode_pins, min_w=0.0001):
         self.driver = DRV8825(dir_pin, step_pin, enable_pin, mode_pins)
         self.driver.SetMicroStep('hardward' ,'fullstep')
+        self.min_w = min_w
+        
+        self.w = 0  # in degrees per second
+        self.terminate = False        
+        threading.Thread(target=self.motor_thread).start()
 
-        self.minimum_w = 0.0001
+    def motor_thread(self):
+        while self.terminate:
+            if abs(self.w) > self.minimum_w:
+                stepdelay = 360/(6400*self.w)
 
-        self.keep_turning = False
-        threading.Thread(target=self.turn_thread).start()
-
-    def turn_thread(self):
-        while True:
-            if self.keep_turning:
-                if abs(self.w) > self.minimum_w:
-                    stepdelay = 360/(6400*self.w)
-                        
-                    self.driver.digital_write(self.step_pin, True)
-                    time.sleep(stepdelay)
-                    self.driver.digital_write(self.step_pin, False)
-                    time.sleep(stepdelay)
+                self.driver.digital_write(self.step_pin, True)
+                time.sleep(stepdelay)
+                self.driver.digital_write(self.step_pin, False)
+                time.sleep(stepdelay)
             else:
                 self.driver.stop()
+        self.driver.stop()
 
     def update_w(self, w):
         self.w = w
-
-    def turn(self, keep_turning):
-        self.keep_turning = keep_turning
+    
+    def terminate(self):
+        self.terminate = True
